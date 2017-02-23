c949c8c8a3e724f1f8c3667fb6de761fbe9e0384
Domenico Vitarella <Domenico Vitarella>
2017-02-23 18:24:07 +0100

    commit

--- src/hashcode2017/Runner.java
+++ src/hashcode2017/Runner.java
@@ -3,7 +3,7 @@ package hashcode2017;
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		System.out.println("Hello world. Staiul");
 	}
 
 }
