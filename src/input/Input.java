f87c9180030e602de3516389b74ba9abd1731151
gianni.mastroscianni <giannisoad12@gmail.com>
2017-02-23 20:43:44 +0100

    input again and again

--- src/input/Input.java
+++ src/input/Input.java
@@ -4,12 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import models.CacheServer;
+import models.EndPoint;
+import models.Request;
 import models.Video;
 
 public class Input {
 
 	private int v, e, r, x, c = 0;
+	private ArrayList<Video> videos = new ArrayList<Video>();
+	private ArrayList<CacheServer> cacheServers = new ArrayList<CacheServer>();
+	private ArrayList<EndPoint> endpoints = new ArrayList<EndPoint>();
+	private ArrayList<Request> requests = new ArrayList<Request>();
 
 	public void getData(String path) throws IOException {
 		File file = new File(path);
@@ -18,16 +27,50 @@ public class Input {
 		String[] values = line.split(" ");
 		this.v = Integer.parseInt(values[0]);
 		this.e = Integer.parseInt(values[1]);
-		this.r = Character.getNumericValue(line.charAt(4));
-		this.c = Character.getNumericValue(line.charAt(6));
-		this.x = Character.getNumericValue(line.charAt(8));
+		this.r = Integer.parseInt(values[2]);
+		this.c = Integer.parseInt(values[3]);
+		this.x = Integer.parseInt(values[4]);
 
+		// Videos
 		line = br.readLine();
 		String[] splitted = line.split(" ");
 		int i = 0;
 		for (i = 0; i < splitted.length; i++) {
-			Video v = new Video(i, Integer.parseInt(splitted[i]));
+			Video v = new Video(i);
+			v.setDim(Integer.parseInt(splitted[i]));
+			videos.add(v);
 			System.out.println(i + " " + Integer.parseInt(splitted[i]));
+		}
+
+		// Endpoints
+		int endpoint = 0;
+		while (br.ready() && endpoint < this.getE()) {
+			line = br.readLine();
+			String[] endp = line.split(" ");
+			EndPoint e = new EndPoint(endpoint);
+			e.setLatencyDC(Integer.parseInt(endp[0]));
+			int numCaches = Integer.parseInt(endp[1]);
+			HashMap<CacheServer, Integer> map = new HashMap<>();
+			for (int k = 0; k < numCaches; k++) {
+				line = br.readLine();
+				String[] cache = line.split(" ");
+				CacheServer cs = new CacheServer(Integer.parseInt(cache[0]), this.getX());
+				map.put(cs, Integer.parseInt(cache[1]));
+				e.setLatenciesCS(map);
+			}
+
+		}
+
+		// Requests
+		int request = 0;
+		while (br.ready() && request < this.getR()) {
+			line = br.readLine();
+			String[] req = line.split(" ");
+			for (int f = 0; f < this.getR(); f++) {
+				Request r = new Request(new Video(Integer.parseInt(req[0])), new EndPoint(parseInt(req[1])),
+						Integer.parseInt(req[2]));
+			}
+
 		}
 
 		br.close();
@@ -57,8 +100,6 @@ public class Input {
 		Input i = new Input();
 		try {
 			i.getData("kittens.in");
-			System.out.println(i.getV());
-			System.out.println(i.getE());
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
